f11e2145059c36444bd8f3aa50d4e0eb64cea150
ADRIAN SKAFTUN <skaftunen@gmail.com>
2023-07-23 23:42:04 +0200

    feat: updated to OOP code and made file change happen immediately
    Signed-off-by: ADRIAN SKAFTUN <skaftunen@gmail.com>

--- main.py
+++ main.py
@@ -1,67 +1,67 @@
-from tkinter import *
+from tkinter import Tk, OptionMenu, StringVar, Text
 import hexInfo
 
-class Main(object):
+OPTIONS_INFO = {
+    "Windows NTFS - Boot Sector": (hexInfo.windowsNTFSVBR, hexInfo.windowsNTFSVBRInfo),
+    "Windows NTFS - MFT File Record": (hexInfo.windowsNTFSMFTFileRecord, hexInfo.windowsNTFSMFTInfo),
+}
+
+class DropdownOption:
+    def __init__(self, master, callback):
+        self.master = master
+        self.clicked = StringVar()
+        self.dropdownOpts = list(OPTIONS_INFO.keys())
+        self.clicked.set(self.dropdownOpts[0])
+        self.drop = OptionMenu(master, self.clicked, *self.dropdownOpts, command=callback)
+        self.drop.grid(row=0, column=0)
+
+class TextWidget:
     def __init__(self, master):
         self.master = master
+        self.textWidget = Text(master, width=47, height=54, font=('Courier 10 bold'), bd=4, padx=5)
+        self.popupText = Text(master, width=48, height=48, font=('Courier 12 bold'), bd=4, padx=5)
+        self.textWidget.grid(row=1, column=0, columnspan=2)
+        self.popupText.grid(row=1, column=2)
 
-        dropdownOpts = [
-            "Windows NTFS - Boot Sector",
-            "Windows NTFS - MFT File Record"
-        ]
+    def highlight_tag(self, tag, color):
+        self.textWidget.tag_configure(tag, background=color)
 
-        clicked = StringVar()
-        clicked.set(dropdownOpts[0])
+    def update_popup_text(self, text, tag):
+        self.popupText.delete("1.0", "end")
+        self.popupText.insert("1.0", text, (tag,))
 
-        drop = OptionMenu(master, clicked, *dropdownOpts)
-        drop.grid(row=0, column=0)
-        #showSelectedBtn = Button(master, text="Show record", command=lambda: showSelection()).grid(row=0, column=1)
-        textWidget = Text(master, width=47, height=54, font=('Courier 10 bold'), bd=4, padx=5)
-        popupText = Text(master, width=48, height=48, font=('Courier 12 bold'), bd=4, padx=5)
-        textWidget.grid(row=1, column=0, columnspan=2)
-        popupText.grid(row=1, column=2)
+class Main:
+    def __init__(self, master):
+        self.master = master
+        self.text_widget = TextWidget(master)
+        self.dropdown = DropdownOption(master, self.showSelection)
+        self.showSelection()
 
-        def iterSelection(mySelection):
-            for key in mySelection:
-                tag = key
-                text = mySelection[key][0]
-                textWidget.insert('end', text, (tag,))
+    def iterSelection(self, mySelection):
+        for key in mySelection:
+            tag = key
+            text = mySelection[key][0]
+            self.text_widget.textWidget.insert('end', text, (tag,))
+            self.text_widget.textWidget.tag_configure(tag)
+            self.text_widget.textWidget.tag_bind(tag, "<Enter>",
+                                                 lambda event, currentTag=tag, popup=mySelection[key][1]: self.popItUp(popup, 'red', currentTag))
+            self.text_widget.textWidget.tag_bind(tag, "<Leave>",
+                                                 lambda event, currentTag=tag: self.popItUp(OPTIONS_INFO[self.dropdown.clicked.get()][1], 'white', currentTag))
 
-                textWidget.tag_configure(tag)
-                textWidget.tag_bind(tag, "<Enter>",
-                                    lambda event, currentTag=tag, popup=mySelection[key][1]: popItUp(popup, 'red', currentTag))
-                textWidget.tag_bind(tag, "<Leave>",
-                                    lambda event, currentTag=tag: popItUp(showSelection(), 'white', currentTag))
+    def popItUp(self, text, color, currTag):
+        self.text_widget.update_popup_text(text, currTag)
+        self.text_widget.highlight_tag(currTag, color)
 
+    def showSelection(self, *args):
+        currentSelection = self.dropdown.clicked.get()
+        self.text_widget.textWidget.delete('1.0', 'end')
+        selection_info = OPTIONS_INFO[currentSelection]
+        self.iterSelection(selection_info[0])
+        self.text_widget.update_popup_text(selection_info[1], None)
 
-            # function to delete and then insert the popuptext.
-            def popItUp(text, color, currTag):
-                popupText.delete("1.0", "end")
-                popupText.insert("1.0", text, (currTag,))
-                textWidget.tag_configure(currTag, background=color)
-
-
-        def showSelection():
-            currentSelection = clicked.get()
-            textWidget.delete('1.0', 'end')
-            if currentSelection == "Windows NTFS - Boot Sector":
-                iterSelection(hexInfo.windowsNTFSVBR)
-                popupText.delete('1.0', 'end')
-                popupText.insert("1.0", hexInfo.windowsNTFSVBRInfo)
-                return hexInfo.windowsNTFSVBRInfo
-            elif currentSelection == "Windows NTFS - MFT File Record":
-                iterSelection(hexInfo.windowsNTFSMFTFileRecord)
-                popupText.delete('1.0', 'end')
-                popupText.insert("1.0", hexInfo.windowsNTFSMFTInfo)
-                return hexInfo.windowsNTFSMFTInfo
-        showSelection()
-
-
-
-
-root = Tk()
-app = Main(root)
-root.title("Poppetypop")
-root.geometry("900x900+500+50")
-
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    app = Main(root)
+    root.title("Poppetypop")
+    root.geometry("900x900+500+50")
+    root.mainloop()
